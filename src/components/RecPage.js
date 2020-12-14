ddf40919b8cd460635ad08d3bd95d680d8a72be5
Nathan Roth <nathanrth15@gmail.com>
2020-12-13 19:34:13 -0500

    fixed redirect bug pt.2

--- src/components/RecPage.js
+++ src/components/RecPage.js
@@ -1,10 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-const RecPage = (props) => {
+const RecPage = ({ searchComplete, recSearchComplete, results }) => {
+  if (!recSearchComplete) {
+    return <Redirect to="/search" />;
+  }
+
   return <div>yoyoyo</div>;
 };
 
-RecPage.propTypes = {};
+RecPage.propTypes = {
+  searchComplete: PropTypes.bool,
+  results: PropTypes.object,
+  recSearchComplete: PropTypes.bool,
+};
 
-export default RecPage;
+const mapStatetoProps = (state) => ({
+  searchComplete: state.search.searchComplete,
+  results: state.search.results,
+  recSearchComplete: state.recs.searchComplete,
+});
+export default connect(mapStatetoProps)(RecPage);
